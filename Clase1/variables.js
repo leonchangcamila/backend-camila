de2875c72bde86d4d96420129b8d732315780e31
leonchangcamila <camila.iurd12@gmail.com>
2022-04-21 16:26:46 -0300

    Clase 1

---
+++ Clase1/variables.js
@@ -0,0 +1,23 @@
+// Desafio generico - Clase 1
+let nombre = 'lucas';
+let edad = 28;
+let precio = 99.90;
+let misSeriesFavoritas= ['Dark','Mr.Robot','Castlevania'];
+let misPeliculas= [
+        {
+            nombre: 'Mision Imposible: fallout',
+            ano: 2019,
+            protagonistas: ['Tom Cruise','Henry']
+        }
+    ];
+
+console.log(nombre);
+console.log(edad);
+console.log(precio);
+console.log(misSeriesFavoritas);
+console.log(misPeliculas);
+
+edad = edad +1;
+console.log(edad);
+misSeriesFavoritas.push('Lost');
+console.log(misSeriesFavoritas);
